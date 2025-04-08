1306c825ca44bd9cdca8606b5c3d60e01422e307
mischelll <mishel.ivanovv@gmail.com>
2025-04-08 12:46:43 +0300

    fe: modify .gitignore, get env var for be url

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const apiUrl = import.meta.env.VITE_BACKEND_USER_SERVICE_URL;
+
 export const fetchCurrentUser = async () => {
-    const res = await axios.get("http://localhost:8080/api/users/me");
+    const res = await axios.get(apiUrl + "/api/users/me");
     return res.data;
 };
 
