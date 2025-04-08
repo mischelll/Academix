1b41c94b3bec29c356115917b18eb28f567b376c
mischelll <mishel.ivanovv@gmail.com>
2025-04-08 17:07:36 +0300

    fe: add env user-service url

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const apiUrl = import.meta.env.VITE_BACKEND_USER_SERVICE_URL;
+const apiUrl = import.meta.env.VITE_BACKEND_LOCAL_USER_SERVICE_URL;
 
 export const fetchCurrentUser = async () => {
-    const res = await axios.get(apiUrl + "/api/users/me");
+    const res = await axios.get(apiUrl + "/users/me");
     return res.data;
 };
 
