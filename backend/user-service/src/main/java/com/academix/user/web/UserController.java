55ce5800c670c79ce538d373c9f16879a4a4bf10
mischelll <mishel.ivanovv@gmail.com>
2025-04-11 13:19:18 +0300

    be: create protected endpoint

--- backend/user-service/src/main/java/com/academix/user/web/UserController.java
+++ backend/user-service/src/main/java/com/academix/user/web/UserController.java
@@ -1,14 +1,18 @@
 package com.academix.user.web;
 
+import com.academix.user.dao.User;
 import com.nimbusds.jose.shaded.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,6 +25,13 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<UserDTO> getMe() {
         logger.info("GET /me");
+        return ResponseEntity.ok(new UserDTO("me"));
+    }
+
+    @GetMapping("/protected/me")
+    public ResponseEntity<UserDTO> getProtectedMe(@AuthenticationPrincipal String userId) {
+        logger.info("GET /protected/me");
+        logger.info(userId);
         return ResponseEntity.ok(new UserDTO("me"));
     }
 }
