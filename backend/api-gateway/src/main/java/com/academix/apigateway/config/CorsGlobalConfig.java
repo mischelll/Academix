849321478b2f76b4d29704628c1f762c6a4b719b
mischelll <mishel.ivanovv@gmail.com>
2025-04-16 18:43:01 +0300

    be: add api-gateway

---
+++ backend/api-gateway/src/main/java/com/academix/apigateway/config/CorsGlobalConfig.java
@@ -0,0 +1,26 @@
+//package com.academix.apigateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class CorsGlobalConfig {
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.setAllowedOriginPatterns(List.of("http://localhost:5173")); // your frontend
+//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        config.setAllowedHeaders(List.of("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
---
+++ backend/api-gateway/src/main/java/com/academix/apigateway/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.academix.apigateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+
+@Configuration
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .pathMatchers("/oauth2/**", "/login/oauth2/**", "/api/users/me").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(@Value("${jwt.secret}") String secret) {
+        SecretKey key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        return NimbusReactiveJwtDecoder.withSecretKey(key).build();
+    }
+}
