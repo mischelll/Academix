e4d0802dc405deb6672ae8416dc7f4a502a80dc2
mischelll <mishel.ivanovv@gmail.com>
2025-04-22 00:32:18 +0300

    be: cors api-gateway reconfiguration

--- backend/api-gateway/src/main/java/com/academix/apigateway/security/SecurityConfig.java
+++ backend/api-gateway/src/main/java/com/academix/apigateway/security/SecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -18,12 +20,16 @@ import javax.crypto.spec.SecretKeySpec;
 public class SecurityConfig  {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, CorsWebFilter corsWebFilter) {
         http
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(corsWebFilter, SecurityWebFiltersOrder.CORS)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .pathMatchers("/oauth2/**", "/login/oauth2/**", "/api/users/me").permitAll()
+                        .pathMatchers("/oauth2/**",
+                                "/login/oauth2/**",
+                                "/api/users/me",
+                                "/api/auth/refresh").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
