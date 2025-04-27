92d0c238abdd7c6f207d8c49f24fd18f2f7db49e
mischelll <mishel.ivanovv@gmail.com>
2025-04-27 17:56:27 +0300

    be: create curriculum-service

---
+++ backend/curriculum-service/src/main/java/com/academix/curriculumservice/CurriculumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.academix.curriculumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurriculumServiceApplication.class, args);
+    }
+
+}
---
+++ backend/curriculum-service/src/test/java/com/academix/curriculumservice/CurriculumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academix.curriculumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
