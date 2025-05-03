6869c524946fed143ef381098a89aa7fb3f065b1
mischelll <mishel.ivanovv@gmail.com>
2025-05-03 23:11:18 +0300

    be: enable method security

--- backend/curriculum-service/src/main/java/com/academix/curriculumservice/security/SecurityConfig.java
+++ backend/curriculum-service/src/main/java/com/academix/curriculumservice/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -16,6 +17,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
