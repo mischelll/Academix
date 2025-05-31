da19acd0189527dc5fb84e03e047ebf8c0806990
mischelll <mishel.ivanovv@gmail.com>
2025-05-31 14:32:12 +0300

    be: add notification service

---
+++ backend/notification-service/src/main/java/com/academix/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.academix.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ backend/notification-service/src/test/java/com/academix/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academix.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
